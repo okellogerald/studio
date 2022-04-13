14fd2a162ab688cb2a6decab459037eea6529187
okellogeralddev@gmail.com <okellogeralddev@gmail.com>
2022-04-13 21:22:08 +0300

    handled video overlay animation

--- lib/pages/video_page.dart
+++ lib/pages/video_page.dart
@@ -2,10 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:silla_studio/manager/video_page/models/video_details.dart';
 import 'package:silla_studio/manager/video_page/providers.dart';
 import 'package:silla_studio/manager/video_page/video_state_notifier.dart';
+import 'package:silla_studio/widgets/video_overlay.dart';
 import 'package:video_player/video_player.dart';
-
 import '../source.dart';
-import '../widgets/video_action_controls.dart';
 
 class VideoPage extends ConsumerStatefulWidget {
   const VideoPage({Key? key}) : super(key: key);
@@ -32,63 +31,49 @@ class _VideoPageState extends ConsumerState<VideoPage> {
     final videoState = ref.watch(videoStateNotifierProvider);
 
     return SafeArea(
-      child: Scaffold(
-          body: videoState.when(
-        initial: () => Container(),
-        loading: _buildLoading,
-        content: () => _buildVideoPlayer(),
-        error: _buildError,
-      )),
-    );
+        child: Scaffold(
+            body: videoState.when(
+      initial: () => Container(),
+      loading: _buildLoading,
+      content: () => _buildVideoPlayer(),
+      error: _buildError,
+    )));
   }
 
   Widget _buildLoading(String? message) {
-    return Container(
-        height: 260.dh,
-        decoration: BoxDecoration(
-            color: Colors.white,
-            image: DecorationImage(image: NetworkImage(videoDetails.image))),
+    return AspectRatio(
+        aspectRatio: 1.78,
         child: Container(
-            height: 260.dh,
-            width: double.maxFinite,
-            color: Colors.white.withOpacity(.5),
-            child: AppLoadingIndicator(message)));
+            decoration: BoxDecoration(
+                color: Colors.white,
+                image:
+                    DecorationImage(image: NetworkImage(videoDetails.image))),
+            child: Container(
+                padding:
+                    EdgeInsets.symmetric(horizontal: 20.dw, vertical: 15.dw),
+                color: Colors.white,
+                child: AppLoadingIndicator(message))));
   }
 
   Widget _buildError(String message) {
-    return Container(
-        height: 260.dh,
-        color: Colors.white,
-        alignment: Alignment.center,
-        padding: EdgeInsets.symmetric(horizontal: 15.dw),
-        child: AppText(message,
-            color: AppColors.error, alignment: TextAlign.center));
+    return AspectRatio(
+        aspectRatio: 1.78,
+        child: Container(
+            color: Colors.white,
+            alignment: Alignment.center,
+            padding: EdgeInsets.symmetric(horizontal: 15.dw),
+            child: AppText(message,
+                color: AppColors.error, alignment: TextAlign.center)));
   }
 
   Widget _buildVideoPlayer() {
     final controller = ref.watch(videoControllerProvider);
     return Center(
-      child: AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
+        child: AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: Stack(alignment: Alignment.bottomCenter, children: [
               VideoPlayer(controller),
-              const VideoActionControls(),
-              Positioned(top: 20.dh, child: _buildTitle()),
-            ],
-          )),
-    );
-  }
-
-  Widget _buildTitle() {
-    final orientationMode = ref.watch(orientationModeProvider);
-    return orientationMode == Orientation.landscape
-        ? Container(
-            padding: EdgeInsets.symmetric(horizontal: 20.dw),
-            color: Colors.black,
-            child: AppText(videoDetails.title,
-                size: 16.dw, color: AppColors.onSecondary))
-        : Container();
+              VideoPlayerOverlay(videoDetails.title)
+            ])));
   }
 }
